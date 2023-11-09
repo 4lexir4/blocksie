d6cdfbe318d13e9da8e05d36742d11df4edea73f
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-11-09 20:41:58 +1100

    creating private key from seed and string, and the corresonding tests

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -1,8 +1,14 @@
 package crypto
 
 import (
-	"github.com/stretchr/testify/assert"
+	//  Needed to generate the seed below
+	//"crypto/rand"
+	//"encoding/hex"
+	//"fmt"
+	//"io"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGeneratePrivateKey(t *testing.T) {
@@ -11,6 +17,28 @@ func TestGeneratePrivateKey(t *testing.T) {
 
 	pubKey := prvKey.Public()
 	assert.Equal(t, len(pubKey.Bytes()), pubKeyLen)
+}
+
+func TestNewPrivateKeyFromString(t *testing.T) {
+	var (
+		seed       = "1053166da15d6da372a520d99a631e7bb4dfcb1d9089b458fd25ce93387dd4bb"
+		prvKey     = NewPrivateKeyFromString(seed)
+		addressStr = "e911ea8baa03eca3d4009ee7ec334fa1a42a31ac"
+	)
+
+	// How the seed above was generated:
+	//seed := make([]byte, 32)
+	//io.ReadFull(rand.Reader, seed)
+	//fmt.Println(hex.EncodeToString(seed))
+
+	assert.Equal(t, prvKeyLen, len(prvKey.Bytes()))
+
+	// How to get address string:
+	//address := prvKey.Public().Address()
+	//fmt.Println(address)
+
+	address := prvKey.Public().Address()
+	assert.Equal(t, addressStr, address.String())
 }
 
 func TerstPrivateKeySign(t *testing.T) {
