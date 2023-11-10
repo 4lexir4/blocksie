dffc6c30f4181b92b510cafb98ed7025066c991d
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-11-10 13:13:42 +1100

    adding tests for signing blocks

--- types/block.go
+++ types/block.go
@@ -3,10 +3,15 @@ package types
 import (
 	"crypto/sha256"
 
+	"github.com/4lexir4/blocksie/crypto"
 	"github.com/4lexir4/blocksie/proto"
 	//pb "github.com/golang/protobuf/proto"
 	pb "google.golang.org/protobuf/proto"
 )
+
+func SignBlock(pk *crypto.PrivateKey, b *proto.Block) *crypto.Signature {
+	return pk.Sign(HashBlock(b))
+}
 
 func HashBlock(block *proto.Block) []byte {
 	b, err := pb.Marshal(block)
