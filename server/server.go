004cca2043f4a618d722233deadf178ea1be79fd
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-11-14 09:40:43 +1100

    server and readme update

--- server/server.go
+++ server/server.go
@@ -1,1 +1,15 @@
 package server
+
+import (
+	"context"
+
+	"github.com/4lexir4/blocksie/proto"
+)
+
+type Node struct {
+	proto.UnimplementedNodeServer
+}
+
+func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*proto.None, error) {
+	return nil, nil
+}
