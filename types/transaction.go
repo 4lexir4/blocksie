c596a87fe7b264c2791cd602ac233bddb87832c1
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-11-13 16:39:39 +1100

    sign transaction

--- types/transaction.go
+++ types/transaction.go
@@ -3,10 +3,15 @@ package types
 import (
 	"crypto/sha256"
 
+	"github.com/4lexir4/blocksie/crypto"
 	"github.com/4lexir4/blocksie/proto"
 
 	pb "google.golang.org/protobuf/proto"
 )
+
+func SignTransaction(pk *crypto.PrivateKey, tx *proto.Transaction) *crypto.Signature {
+	return pk.Sign(HashTransaction(tx))
+}
 
 func HashTransaction(tx *proto.Transaction) []byte {
 	b, err := pb.Marshal(tx)
